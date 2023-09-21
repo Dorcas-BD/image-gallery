9a05835ad16f3b33b622c4890b7c5bf5748b69ca
Dorcas Bamisile <bamisiledorcas@gmail.com>
2023-09-21 15:23:21 +0100

    Remove Login

--- app/page.js
+++ app/page.js
@@ -1,14 +1,12 @@
 "use client";
 
 import React from "react";
-import Login from "./Login/Login";
 import GalleryHomepage from "./GalleryHomePage/Page";
 
 const HomePage = () => {
   return (
     <div>
       <GalleryHomepage />
-      {/* <Login /> */}
     </div>
   );
 };
